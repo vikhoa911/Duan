0d98e274923a070365e7824586713e807f643af4
vinhntph51382 <vinhntph51382>
2024-11-23 14:53:18 +0700

    sửa css

--- models/danhmuc.php
+++ models/danhmuc.php
@@ -5,7 +5,7 @@ function update_danhmuc($id, $tenloai){
     $sql="update danh_muc set name='$tenloai' where id=$id";
     pdo_execute($sql);
 }
-<?php 
+
 function them_danh_muc($ten_danh_muc){
     $sql="insert into danh_muc(ten_danh_muc) values('$ten_danh_muc')";
     pdo_execute($sql);
