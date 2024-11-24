a37e850429fd9d24f28ba055c7b0c4116c2e072d
vikhoa911 <khoagvph51310@gmail.com>
2024-11-24 23:53:42 +0700

    sửa lại model danh mục

--- models/danhmuc.php
+++ models/danhmuc.php
@@ -1,7 +1,7 @@
 <?php
 
-function update_danh_muc($id_danh_muc, ten_danh_muc){
-    $sql="update danh_muc set ten_danh_muc='ten_danh_muc' where $id_danh_muc=$id_danh_muc";
+function update_danh_muc($id_danh_muc, $ten_danh_muc){
+    $sql="update danh_muc set ten_danh_muc='$ten_danh_muc' where $id_danh_muc=$id_danh_muc";
     pdo_execute($sql);
 }
 function them_danh_muc($ten_danh_muc){
