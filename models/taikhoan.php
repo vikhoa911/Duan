7098d2d9158fcc60f551adf850d3497960d862d3
vikhoa911 <khoagvph51310@gmail.com>
2024-11-29 01:17:07 +0700

    Update taikhoan.php

--- models/taikhoan.php
+++ models/taikhoan.php
@@ -4,6 +4,7 @@ function dangky($ten_dang_nhap, $email, $mat_khau) {
     $sql = "INSERT INTO `tai_khoan` (ten_dang_nhap, email, mat_khau) VALUES ('$ten_dang_nhap', '$email', '$mat_khau');";
     pdo_execute($sql);
 }
+// Hàm đăng nhập tài khoản
 function check_user($ten_dang_nhap, $mat_khau){
     $sql="select * from tai_khoan where ten_dang_nhap='".$ten_dang_nhap."' AND  mat_khau='".$mat_khau."'";
     $sp=pdo_query_one($sql);
@@ -12,22 +13,27 @@ function check_user($ten_dang_nhap, $mat_khau){
 }
 
 
-function update_tai_khoan($id_tai_khoan, $ten_dang_nhap, $mat_khau, $email, $dia_chi, $so_dien_thoai){
-    $sql = "UPDATE tai_khoan SET ten_dang_nhap='$ten_dang_nhap', mat_khau='$mat_khau', email='$email', dia_chi='$dia_chi', so_dien_thoai='$so_dien_thoai' WHERE id_tai_khoan=$id_tai_khoan";
-    pdo_execute($sql);
+// Hàm cập nhật tài khoản
+function update_tai_khoan($id_tai_khoan, $ten_dang_nhap, $email, $dia_chi, $so_dien_thoai, $vai_tro) {
+    $sql = "UPDATE tai_khoan SET email = ?, dia_chi = ?, so_dien_thoai = ?, vai_tro = ? WHERE id_tai_khoan = ?";
+    pdo_execute($sql, $email, $dia_chi, $so_dien_thoai, $vai_tro, $id_tai_khoan);
 }
 
+
+// Hàm xóa tài khoản
 function delete_tai_khoan($id_tai_khoan){
     $sql = "DELETE FROM tai_khoan WHERE id_tai_khoan=" . $id_tai_khoan;
     pdo_execute($sql);
 }
 
+// Hàm hiển thị all tài khoản
 function loadall_tai_khoan(){
     $sql="select * from tai_khoan order by id_tai_khoan desc";
     $listtai_khoan=pdo_query($sql);
     return $listtai_khoan;
 }
 
+// Hàm chọn tài khoản
 function loadone_tai_khoan($id_tai_khoan){
     $sql="select * from tai_khoan where id_tai_khoan=".$id_tai_khoan;
     $tai_khoan=pdo_query_one($sql);
