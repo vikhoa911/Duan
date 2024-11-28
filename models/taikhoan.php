05842949f324303a5508a9d936cb3f19721924fb
vinhntph51382 <vinhntph51382>
2024-11-28 14:49:15 +0700

    dki

--- models/taikhoan.php
+++ models/taikhoan.php
@@ -3,10 +3,9 @@ include_once "models/config.php";
 
 // Hàm đăng ký tài khoản
 function dangky($ten_dang_nhap, $email, $mat_khau) {
-    $sql = "INSERT INTO tai_khoan (ten_dang_nhap, email, mat_khau) VALUES (:ten_dang_nhap, :email, :mat_khau)";
-    pdo_execute($sql, $ten_dang_nhap, $email, $mat_khau);
+    $sql = "INSERT INTO `tai_khoan` (ten_dang_nhap, email, mat_khau) VALUES ('$ten_dang_nhap', '$email', '$mat_khau');";
+    pdo_execute($sql);
 }
-
 function check_user($ten_dang_nhap, $mat_khau){
     $sql="select * from tai_khoan where ten_dang_nhap='".$ten_dang_nhap."' AND  mat_khau='".$mat_khau."'";
     $sp=pdo_query_one($sql);
